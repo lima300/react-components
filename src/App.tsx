f538ad2d856b003305205249f4d2dfcce0fcf2b0
Otávio Lima <45007067+lima300@users.noreply.github.com>
2023-08-27 22:01:59 -0300

    :rocket: Media Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // import { Accordion } from "./components/accordion/Accordion";
 // import { Card } from "./components/card/Card";
-import Sidebar from "./components/sidebar/Sidebar";
+import { MediaPlayer } from "./components/mediaPlayer/MediaPlayer";
+// import Sidebar from "./components/sidebar/Sidebar";
 // import { Carousel } from "./components/carousel/Carousel";
 
 function App() {
   return (
     <div className="sidebar-container">
-      <Sidebar />
+      <MediaPlayer />
     </div>
   );
 }
