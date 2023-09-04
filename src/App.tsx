5aae0e1640fa57d19f822de49630be2c3fed8262
Otávio Lima <45007067+lima300@users.noreply.github.com>
2023-09-04 20:36:18 -0300

    :rocket: Liquid button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 // import { Accordion } from "./components/accordion/Accordion";
 // import { Card } from "./components/card/Card";
-import { MediaPlayer } from "./components/mediaPlayer/MediaPlayer";
+import { LiquidButton } from "./components/buttons/liquidButton/LiquidButton";
+// import { MediaPlayer } from "./components/mediaPlayer/MediaPlayer";
 // import Sidebar from "./components/sidebar/Sidebar";
 // import { Carousel } from "./components/carousel/Carousel";
 
 function App() {
-  return (
-    <div className="sidebar-container">
-      <MediaPlayer />
-    </div>
-  );
+  return <LiquidButton />;
 }
 
 export default App;
---
+++ src/components/buttons/liquidButton/LiquidButton.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export const LiquidButton = () => {
+  return (
+    <a href="#">
+      <span>button</span>
+      <div className="liquid"></div>
+    </a>
+  );
+};
