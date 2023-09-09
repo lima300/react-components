d91ddee2d05e36e278c1961a8ac47ead3f8b3ee3
Otavio Lima <otaviolima@Otavios-MacBook-Pro.local>
2023-09-09 16:27:09 -0300

    :rocket: changes to yarn and creates path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-// import { Accordion } from "./components/accordion/Accordion";
-// import { Card } from "./components/card/Card";
-// import { LiquidButton } from "./components/buttons/liquidButton/LiquidButton";
-import { Login } from "./components/login/Login";
-// import { ToggleSwitch } from "./components/toggleSwitch/ToggleSwitch";
-// import { MediaPlayer } from "./components/mediaPlayer/MediaPlayer";
-// import Sidebar from "./components/sidebar/Sidebar";
-// import { Carousel } from "./components/carousel/Carousel";
+import { Accordion } from "@/components/accordion/Accordion";
+import { Card } from "@/components/card/Card";
+import { LiquidButton } from "@/components/buttons/liquidButton/LiquidButton";
+import { Login } from "@/components/login/Login";
+import { ToggleSwitch } from "@/components/toggleSwitch/ToggleSwitch";
+import { MediaPlayer } from "@/components/mediaPlayer/MediaPlayer";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { Carousel } from "@/components/carousel/Carousel";
 
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <Accordion />
+      <Card />
+      <LiquidButton />
+      <ToggleSwitch />
+      <MediaPlayer />
+      <Sidebar />
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
